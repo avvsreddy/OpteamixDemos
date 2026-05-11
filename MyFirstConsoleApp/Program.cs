ac9f379df8714827dc5d1f541aabde2e003424f1
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-05-11 18:02:01 +0530

    Properties demo

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+        // Class Members
+            // State - Data
+                // Fields/Variables
+                // Properties - IMP
+
+            // Behavior - Action
+                // Methods - IMP
+                // Constructor
+                // Destructor - Never Use
+                // Events
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to my World!");
+            Employee e = new Employee();
+            e.empNo = 111;
+            e.empName = "Sachin";
+            e.salary = -890000;
+           
         }
+
+       
+    }
+
+
+    class Employee
+    {
+        public int empNo;
+        public string empName;
+         double salary;
     }
 }

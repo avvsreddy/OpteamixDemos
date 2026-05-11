698113542d9c3de6ebc2d7fbeb756a594865c574
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-05-11 14:04:30 +0530

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to my World!");
+        }
+    }
+}

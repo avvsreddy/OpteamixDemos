1a3534a8768500a99e23f4a4877b60d2702c6bfb
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-05-13 12:59:43 +0530

    ide case study

---
+++ IDECaseStudy.ConsoleApp/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace IDECaseStudy.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDE ide = new IDE();
+            LangC c = new LangC();
+            LangCSharp cs = new LangCSharp();
+            LangJava java = new LangJava();
+
+            ide.Java = java;
+            ide.CS = cs;
+            ide.C = c;
+
+            ide.Work();
+        }
+    }
+
+    class IDE
+    {
+        public LangC C { get; set; }
+        public LangCSharp CS { get; set; }
+        public LangJava Java { get; set; }
+        public SomeLang SL { get; set; }
+
+        public void Work() 
+        {
+            Console.WriteLine(C.GetName());
+            Console.WriteLine(C.GetParadigm());
+            Console.WriteLine(C.GetUnit());
+            Console.WriteLine("-------------");
+            Console.WriteLine(CS.GetName());
+            Console.WriteLine(CS.GetParadigm());
+            Console.WriteLine(CS.GetUnit());
+            Console.WriteLine("-------------");
+            Console.WriteLine(Java.GetName());
+            Console.WriteLine(Java.GetParadigm());
+            Console.WriteLine(Java.GetUnit());
+            Console.WriteLine("-------------");
+
+        }
+
+    }
+
+
+    class SomeLang
+    {
+        public void SomeMethod() { }
+    }
+
+    class LangCSharp
+    {
+        public string GetParadigm()
+        {
+            return "Object Oriented";
+        }
+        public string GetUnit()
+        {
+            return "Class";
+        }
+        public string GetName()
+        {
+            return "C Sharp";
+        }
+    }
+    class LangJava
+    {
+        public string GetParadigm()
+        {
+            return "Object Oriented";
+        }
+        public string GetUnit()
+        {
+            return "Class";
+        }
+        public string GetName()
+        {
+            return "Java";
+        }
+    }
+    class LangC
+    {
+        public string GetParadigm()
+        {
+            return "Procedural";
+        }
+        public string GetUnit()
+        {
+            return "Function";
+        }
+        public string GetName()
+        {
+            return "C";
+        }
+    }
+}
---
+++ OOConcepts/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace OOConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Triangle();
+            DrawSomeShape(s);
+
+        }
+
+        static void DrawSomeShape(Shape a)
+        {
+            a.Draw();
+        }
+
+    }
+
+
+    //interface Shape
+    abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw(); // non-polymarphic
+        //{
+        //    Console.WriteLine("Drawing Shape");
+        //}
+        public abstract double CalculateArea();
+        //{
+        //    Console.WriteLine("Calcuating shape area");
+        //    return Width * Height;
+        //}
+    }
+
+    interface ThreeDShape
+    {
+
+    }
+
+    interface OtherInterface { }
+
+    class Rectangle : Shape, ThreeDShape, OtherInterface  // IS-A - Generalization - BAD
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+        public override double CalculateArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Triangle : Shape 
+    {
+
+        override public void Draw()
+        {
+            Console.WriteLine("Drawing Triangle");
+        }
+         override public double CalculateArea() // Method Hiding
+        {
+            Console.WriteLine("Calculating Triangle area");
+            return Width * Height / 2;
+            //double area = base. CalculateArea();
+            //return area / 2;
+        }
+    }
+}

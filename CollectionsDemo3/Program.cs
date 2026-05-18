1ea5bf697805e1d7eaf3f7eed57c2235c717b2ae
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-05-18 13:59:56 +0530

    dp1

---
+++ CollectionsDemo3/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace CollectionsDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 23, 56, 12, 34, 78, 90, 11, 45 };
+
+            numbers.Sort();
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Laptop", Price = 1200.00 },
+                new Product() { Id = 2, Name = "Smartphone", Price = 800.00 },
+                new Product() { Id = 3, Name = "Tablet", Price = 500.00 }
+            };
+
+
+            // Table Products
+            // col: price
+
+            // get all products order by price
+            // SQL: SELECT * FROM Products ORDER BY Price
+
+            var sortedProducts = products.OrderBy(p => p.Price).ToList(); // This will sort the products by price
+            var sortedProductsDescending = products.OrderByDescending(p => p.Price).ToList(); // This will sort the products by price in descending order
+            var bestway = from p in products
+                          orderby p.Price descending
+                          select p;
+
+
+
+            products.Sort(new ProductPriceComparer()); // This will sort the products by price
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"{product.Name}: ${product.Price}");
+            }
+        }
+
+        class ProductPriceComparer : IComparer<Product>
+        {
+            public int Compare(Product x, Product y)
+            {
+                return x.Price.CompareTo(y.Price);
+            }
+        }
+
+        class Product// : IComparable<Product>
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+
+            //public int CompareTo(Product other)
+            //{
+            //     if(this.Price < other.Price)
+            //    {
+            //        return 1;
+            //    }
+            //    else if (this.Price > other.Price)
+            //    {
+            //        return -1;
+            //    }
+            //    else
+            //    {
+            //        return 0;
+            //    }
+            //}
+        }
+    }
+}
---
+++ DesignPatterns.POS/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace DesignPatterns.POS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBill processBill = new ProcessBill();
+            processBill.Process();
+        }
+    }
+
+    class ProcessBill
+    {
+        public void Process()
+        {
+            // Process the bill
+            // Read the barcode,
+            // calculate the total,
+            // apply discounts, etc.
+            double total = 14500.00;
+            // Calculate the Tax
+            //TaxCalculator taxCalculator = new TaxCalculator();
+            KATaxCalculator taxCalculator = new KATaxCalculator();
+            double tax = taxCalculator.CalculateTax(total);
+            Console.WriteLine($"Total: {total}");
+            Console.WriteLine($"Tax: {tax}");
+        }
+    }
+
+    interface ITaxCalculator
+    {
+        double CalculateTax(double total);
+    }
+
+
+    class KATaxCalculator : ITaxCalculator
+    {
+        public double CalculateTax(double total)
+        {
+            Console.WriteLine("Using KA Tax Calculator");
+            // Calculate the tax based on the total
+            int es = 100;
+            int bs = 10;
+            int nd = 10;
+            int cgst = 50;
+            int sgst = 50;
+            double tax = es + bs + nd + cgst + sgst;
+            return tax;
+        }
+    }
+
+    class TNTaxCalculator : ITaxCalculator
+    {
+        public double CalculateTax(double total)
+        {
+            Console.WriteLine("Using TN Tax Calculator");
+            // Calculate the tax based on the total
+            int es = 200;
+            //int bs = 20;
+            int nd = 20;
+            int cgst = 100;
+            int sgst = 100;
+            double tax = es + nd + cgst + sgst;
+            return tax;
+        }
+    }
+
+
+}

d147ce33fbd8f3c1f50b51f0d3610e18f273bb2c
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-05-20 15:51:50 +0530

    del 5

--- DelegatesDemo3/Program.cs
+++ DelegatesDemo3/Program.cs
@@ -10,9 +10,15 @@ namespace DelegatesDemo3
         {
             // open an account
             Account acc1 = new Account();
-            acc1.Deposit(1000);
+            acc1.notify += NotificationService.SendMail; // subscribe to the notification
+            //acc1.Subscribe(NotificationService.SendSMS); // subscribe to the notification
+            acc1.notify += NotificationService.SendSMS; // subscribe to the notification
+            //acc1.notify -= NotificationService.SendSMS; // unsubscribe to the notification
+            acc1.notify += NotificationService.SendWhatsApp; // subscribe to the notification
+            //acc1.Deposit(1000);
+            acc1.notify("A deposit of $1000000000000 has been made to your account.");
             Console.WriteLine(acc1.Balance);
-            acc1.Withdraw(500);
+            //acc1.Withdraw(500);
             Console.WriteLine(acc1.Balance);
 
         }
@@ -23,14 +29,27 @@ namespace DelegatesDemo3
     {
         public int Balance { get; private set; }
 
-        private NotificationService notificationService = new NotificationService();
+        //private NotificationService notificationService = new NotificationService();
+
+        public event Notify notify = null; // new Notify(new NotificationService().SendMail);
+
+        //public void Subscribe(Notify notifyMethod)
+        //{
+        //    notify += notifyMethod;
+        //}
+
+        //public void Unsubscribe(Notify notifyMethod)
+        //{
+        //    notify -= notifyMethod;
+        //}
 
         public void Deposit(int amount)
         {
             Balance += amount;
             // write a logic to send notification to the customer about the deposit
-            notificationService.SendMail($"A deposit of {amount} has been made to your account.");
-            notificationService.SendSMS($"A deposit of {amount} has been made to your account.");
+            //notificationService.SendMail($"A deposit of {amount} has been made to your account.");
+            //notificationService.SendSMS($"A deposit of {amount} has been made to your account.");
+            notify?.Invoke($"A deposit of {amount} has been made to your account.");
         }
 
 
@@ -40,24 +59,33 @@ namespace DelegatesDemo3
         public void Withdraw(int amount)
         {
             Balance -= amount;
-            notificationService.SendMail($"A withdrawal of {amount} has been made from your account.");
-            notificationService.SendSMS($"A withdrawal of {amount} has been made from your account.");
+            //notificationService.SendMail($"A withdrawal of {amount} has been made from your account.");
+            //notificationService.SendSMS($"A withdrawal of {amount} has been made from your account.");
+            notify?.Invoke($"A withdrawal of {amount} has been made from your account.");
         }
 
 
     }
 
 
+    public delegate void Notify(string msg);
+
+
     class NotificationService
     {
-        public void SendMail(string msg)
+        public static void SendMail(string msg)
         {
             Console.WriteLine($"Mail: {msg} ");
         }
 
-        public void SendSMS(string msg)
+        public static void SendSMS(string msg)
         {
             Console.WriteLine($"SMS: {msg} ");
+        }
+
+        public static void SendWhatsApp(string msg)
+        {
+            Console.WriteLine($"WhatsApp: {msg} ");
         }
     }
 }

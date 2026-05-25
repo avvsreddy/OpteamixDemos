d7271b927ce4fd76c4a094f3c23eabb84f7dc3d7
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-05-25 16:26:09 +0530

    Multi Threading labs

---
+++ MTDemo6ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+namespace MTDemo6ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataProducer producer = new DataProducer();
+            //producer.FillData();
+            //producer.FillData();
+            Parallel.Invoke(producer.FillData, producer.FillData);
+            Console.WriteLine(producer.DataStack.Count);
+        }
+    }
+
+
+    class DataProducer
+    {
+        //public Stack<int> DataStack { get; private set; } = new Stack<int>();
+        public ConcurrentStack<int> DataStack { get; private set; } = new ConcurrentStack<int>();
+
+        //[MethodImpl(MethodImplOptions.Synchronized)]
+        public void FillData() 
+        {
+            for (int i = 0; i < 10000000; i++)
+            {
+                //Monitor.Enter(this);
+                //lock (this)
+                //{
+                    DataStack.Push(i);
+                //}
+                //Monitor.Exit(this);
+
+                //Mutex // can be used across process boundaries, but it is slower than lock and Monitor
+                // Semaphore // can be allowd multiple threads to access the critical section
+
+
+            }
+        }
+    }
+}
--- MTDemoWinFormsApp/Form1.cs
+++ MTDemoWinFormsApp/Form1.cs
@@ -10,30 +10,36 @@ namespace MTDemoWinFormsApp
         private void button1_Click(object sender, EventArgs e)
         {
             // red
-            Graphics g = panel1.CreateGraphics();
-            Random rand = new Random();
-            for (int i = 0; i < 1000; i++)
+            new Thread(() =>
             {
-                int x = rand.Next(panel1.Width);
-                int y = rand.Next(panel1.Height);
-                g.DrawRectangle(Pens.Red, x, y, 10, 10);
-                Thread.Sleep(10);
-            }
+                Graphics g = panel1.CreateGraphics();
+                Random rand = new Random();
+                for (int i = 0; i < 1000; i++)
+                {
+                    int x = rand.Next(panel1.Width);
+                    int y = rand.Next(panel1.Height);
+                    g.DrawRectangle(Pens.Red, x, y, 10, 10);
+                    Thread.Sleep(10);
+                }
+            }).Start();
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             // blue
-            Graphics g = panel2.CreateGraphics();
-            Random rand = new Random();
-            for (int i = 0; i < 1000; i++)
+            Task.Factory.StartNew(() =>
             {
-                int x = rand.Next(panel2.Width);
-                int y = rand.Next(panel2.Height);
-                g.DrawRectangle(Pens.Blue, x, y, 10, 10);
-                Thread.Sleep(10);
-            }
+                Graphics g = panel2.CreateGraphics();
+                Random rand = new Random();
+                for (int i = 0; i < 1000; i++)
+                {
+                    int x = rand.Next(panel2.Width);
+                    int y = rand.Next(panel2.Height);
+                    g.DrawRectangle(Pens.Blue, x, y, 10, 10);
+                    Thread.Sleep(10);
+                }
+            });
         }
     }
 }

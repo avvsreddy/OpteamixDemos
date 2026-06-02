0282880ea21d39255500850c6e9cfc1a1b2efdd9
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-06-02 11:09:26 +0530

    api

--- ProductCatelogService.Api/Controllers/ProductsController.cs
+++ ProductCatelogService.Api/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductCatelogService.Data;
 using ProductCatelogService.Domain.Entities;
+using ProductCatelogService.Domain.Repositories;
 
 namespace ProductCatelogService.Api.Controllers
 {
@@ -13,12 +15,33 @@ namespace ProductCatelogService.Api.Controllers
     {
 
         // GET: http://localhost:5000/api/products
-        //[HttpGet]
-        //public List<Product> GetProducts()
-        //{
-        //    // fetch data from database and return to client
-        //}
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            // fetch data from database and return to client
+            IProductsRepository repo = new ProductsRepository();
+            return await repo.GetAllProductsAsync();
+        }
 
+
+        // Design the endpoint uri and method for searching product by id
+        // GET: http://localhost:5000/api/products/1
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            IProductsRepository repo = new ProductsRepository();
+
+            var product = await repo.GetProductByIdAsync(id);
+            if(product == null)
+            {
+                // return 404 not found
+                return NotFound();
+
+            }
+            // return 200 with data
+            return Ok(product);
+        }
 
     }
 }

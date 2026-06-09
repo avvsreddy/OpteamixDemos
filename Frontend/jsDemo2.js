e2a0c8e372784c16bf72bf8a1a6a71c0a1f453a1
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-06-09 11:50:54 +0530

    todolist

--- Frontend/jsDemo2.js
+++ Frontend/jsDemo2.js
@@ -1,11 +1,16 @@
 //var x = 0;
 function Increment() {
     var x = 0;
-    x++;
-    console.log(x);
+    // Closure: A closure is a function that has access to its own scope, the outer function's scope, and the global scope. In this case, the inner function (the one returned by Increment) has access to the variable x defined in the outer function (Increment). This allows the inner function to modify and access x even after Increment has finished executing.
+    return function() {
+        x++;
+        console.log(x);
+    }
 }
 
+var inc = Increment();
 
-Increment();
-Increment();
-Increment();
+inc();
+inc();
+inc();
+

8c834cb6a38a7a41d29ed0d5df8ed44daf589149
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-06-11 10:26:40 +0530

    controll structures

--- Frontend/AngularProjects/my-first-ng-project/src/app/app.ts
+++ Frontend/AngularProjects/my-first-ng-project/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { IProduct } from '../interfaces/IProduct';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +11,40 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   
+products:IProduct[] = [
+  {
+  id:101,
+  name:"I Phone",
+  price:99999,
+  isAvailable:true
+  },
+   {
+  id:102,
+  name:"I Phone X",
+  price:99999,
+  isAvailable:false,
+    //brand:'apple'
 
-title:string = "Welcome to my First Angular Application...!";
-
-
-empName:string = "Ravi";
-salary:number = 75000;
-isActive:boolean=true;
-pictureUrl:string = "https://placehold.net/avatar.svg";
+  },
+   {
+  id:101,
+  name:"Galaxy S26",
+  price:99999,
+  isAvailable:true
+  },
+   {
+  id:101,
+  name:"Oppo M45",
+  price:99999,
+  isAvailable:false
+  },
+   {
+  id:101,
+  name:"I Watch",
+  price:99999,
+  isAvailable:true
+  }
   
-display()
-{
-  alert('hello');
-}
-
+]
 
 }
